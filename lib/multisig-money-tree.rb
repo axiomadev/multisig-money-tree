13336db1498da026aad90baa3dea171a625d1f5f
Korol Alex <korol.sas@gmail.com>
2018-03-16 14:46:05 +0200

    fix gamspec

--- lib/multisig-money-tree.rb
+++ lib/multisig-money-tree.rb
@@ -1,12 +1,12 @@
-require "money-tree"
+require 'money-tree'
 require 'bitcoin'
 
-require "multisig-money-tree/version"
-require "multisig-money-tree/support"
-require "multisig-money-tree/errors"
-require "multisig-money-tree/networks"
-require "multisig-money-tree/node"
-require "multisig-money-tree/master"
+require 'multisig-money-tree/version'
+require 'multisig-money-tree/support'
+require 'multisig-money-tree/errors'
+require 'multisig-money-tree/networks'
+require 'multisig-money-tree/node'
+require 'multisig-money-tree/master'
 
 module MultisigMoneyTree
   
